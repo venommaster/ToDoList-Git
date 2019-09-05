70f61d94ff6fc29570b574288371787cc3dd941a
Rahul BK <rbkrockz92@gmail.com>
2019-09-05 18:30:23 +0530

    Added Login.swift and hello world message

---
+++ ToDoList/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  ToDoList
+//
+//  Created by Krishna Rahul Bhaskarla on 05/09/19.
+//  Copyright © 2019 Gwinyai Nyatsoka. All rights reserved.
+//
+
+import Foundation
--- ToDoList/ToDoDetailsViewController.swift
+++ ToDoList/ToDoDetailsViewController.swift
@@ -22,10 +22,17 @@ class ToDoDetailsViewController: UIViewController {
     
     var toDoIndex: Int!
 
+    let message = "Hello World"
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        
+        print(message)
         
         taskTitleLabel.text = toDoItem.name
         
